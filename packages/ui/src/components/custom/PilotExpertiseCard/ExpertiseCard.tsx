41137fd2943057f012c4056b1e0b639fea9424ad
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2023-12-15 01:40:14 +0530

    Fix: Regarding to Latest Milestone

--- packages/ui/src/components/custom/PilotExpertiseCard/ExpertiseCard.tsx
+++ packages/ui/src/components/custom/PilotExpertiseCard/ExpertiseCard.tsx
@@ -3,7 +3,7 @@
  * Purpose        : Allow clients to select pilot expertises
  * Used in        : workspace-client
  */
-import { useState } from 'react'
+// import { useState } from 'react'
 
 export default function PilotExpertiseCard() {
 
